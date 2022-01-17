833a15ae4fc9a2fba6dbfbc2af53cc702acd7341
nobelios <nobelios@gmail.com>
2022-01-17 17:00:46 +0100

    update avec Gerant

--- src/main/java/com/inti/entities/Gerant.java
+++ src/main/java/com/inti/entities/Gerant.java
@@ -4,16 +4,16 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Gerant implements Serializable{
 	@Id
-	@GeneratedValue (strategy=)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	public Long idGerant;
 	public String nom;
 	public String prenom;
-	
 	
 	
 }
